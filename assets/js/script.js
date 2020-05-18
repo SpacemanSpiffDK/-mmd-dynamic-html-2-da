350e918d3736dc19f19f2784bf975ca44dcbc10c
Dan Høegh <dan.hoegh@gmail.com>
2020-05-18 10:44:15 +0200

    stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
 // JS by Dan Høegh
 // UCN MMD 2020
+
+// function menuOn(){
+//     document.querySelector('header > nav').classList.add('active');
+// }
+
+// function menuOff(){
+//     document.querySelector('header > nav').classList.remove('active');
+// }
+
+function toggleClass(selector, className){
+    document.querySelector(selector).classList.toggle(className);
+}
+
+function toggleClassDeleteBtn(elm, selector, className){
+    document.querySelector(selector).classList.toggle(className);
+    elm.style.display="none";
+}
